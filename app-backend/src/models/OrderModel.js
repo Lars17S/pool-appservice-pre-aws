4042de51ea2492a252b4281a6aca82231efe8c29
Lars17S <efren.garibaldi@gmail.com>
2022-09-06 20:00:13 -0500

    changes in project structure

---
+++ app-backend/src/models/OrderModel.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const CartSchema = new Schema()(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const CartModel = model("Cart", CartSchema);
+
+export default CartModel;
---
+++ app-backend/src/models/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user";
+import authRouter from "./routes/auth";
+import productRouter from "./routes/product";
+import orderRouter from "./routes/order";
+import cartRouter from "./routes/cart";
+import cors from "cors";
+import passport from "passport";
+import cookieSession from "cookie-session";
+import localStrategy from "./passport/passport";
+import stripeRouter from "./routes/stripe";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.EXPRESS_PORT || "5000";
+const mongooseurl = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongooseurl)
+  .then(() => console.log("DB Connection Successful!"));
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3002"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+localStrategy(passport);
+app.use(
+  cookieSession({ name: "session", keys: ["lars"], maxAge: 24 * 60 * 60 * 100 })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/checkout", stripeRouter);
+
+app.listen(port, () => {
+  let listApp = `App listening on port ${port}! (http://localhost:${port})`;
+  return console.log(listApp);
+});
